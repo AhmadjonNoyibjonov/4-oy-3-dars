2e83c7200406ba9296348b7a90085f15907d367c
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-22 00:46:48 +0500

    project finishid

---
+++ js/index.js
@@ -0,0 +1,235 @@
+//  1-masala
+// let A = +prompt("A soni kiritilsin");
+// if (A > 0) {
+//   console.log("Musbat");
+// } else {
+//   console.log("Manfiy");
+// }
+
+// 2-masala
+// let A = +prompt("A soni kiritilsin");
+// if (A % 2 == 1) {
+//     console.log('Toq');
+// } else {
+//     console.log('Juft');
+// }
+
+// 3-masala
+// let A = +prompt("A soni kiritilsin");
+// if (A % 2 == 1) {
+//     console.log('Juft');
+// } else {
+//     console.log('Toq');
+// }
+
+// 4-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// if (A > 2 && B <= 3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 5-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// if (A >= 0 / B < -2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 6-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A <= B && B <= C) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 7-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if ( A < B && B < C) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 8-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// if (A % 2 == 1 && B % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 9-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kiritilsin");
+// if (A % 2 == 1 / B % 2 == 1) {
+//     console.log(true);
+// } else if ( A %2 == 1 && B % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 10-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// if (A % 2 == 1 / B % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 11-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// if (A % 2 ==1 && B % 2 == 1) {
+//     console.log(true);
+// } else if (A % 2 == 0 && B % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 12-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A > 0 && B > 0 && C > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 13-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A > 0 / B > 0 / C > 0) {
+//     console.log(true);
+// } else if (A > 0 && B > 0 / C > 0 && A > 0 / B > 0 && C > 0) {
+//     console.log(true);
+// } else if (A > 0 && B > 0 && C > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 14-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A > 0 / B > 0 / C > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 15-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A > 0 && B > 0 / C > 0 && A > 0 / B > 0 && C > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 16-masala
+// let n = +prompt("n soni musbat butun son kiritilsin");
+// if (n > 9 && n < 100 && n % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 17-masala
+// let n = +prompt("n soni musbat butun son kiritilsin");
+// if (n < 1000 && n >=100 && n % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 18-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A == B / A == C / B == C) {
+//     console.log(true);
+// } else if (A == B && A == C && C == B) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 19-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kieitilsin");
+// let C = +prompt("C soni kieitilsin");
+// if (A + B == 0 / A + C == 0 / B + C == 0) {
+//     console.log(true);
+// } else if (A + B == 0 && A + C == 0 && C + B == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 20-masala
+// let A;
+// let b = +prompt('b soni kiritilsin');
+// let c = +prompt('c soni kiritilsin');
+// let d = +prompt('d soni kiritilsin');
+// A = 100*b + 10*c + d;
+// if (b != c && c != d && b != d) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 21-masala
+// let A;
+// let b = +prompt('b soni kiritilsin');
+// let c = +prompt('c soni kiritilsin');
+// let d = +prompt('d soni kiritilsin');
+// A = 100*b + 10*c + d;
+// if (b + 1 == c && c + 1 == d) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 22-masala
+// let A;
+// let b = +prompt('b soni kiritilsin');
+// let c = +prompt('c soni kiritilsin');
+// let d = +prompt('d soni kiritilsin');
+// A = 100*b + 10*c + d;
+// if (b + 1 == c && c + 1 == d) {
+//     console.log(true);
+// } else if (b - 1 == c && c - 1 == d) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 23-masala
+// let A;
+// let b = +prompt('b soni kiritilsin');
+// let c = +prompt('c soni kiritilsin');
+// let d = +prompt('d soni kiritilsin');
+// A = 100*b + 10*c + d;
+// if (b == d / b == c && c == d & b == d) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
